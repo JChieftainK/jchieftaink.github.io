7f2a5761a7eb7567199dae51cc1b015134826925
Justin Kennedy <justinjaykennedy@gmail.com>
2019-06-08 14:51:38 -0500

    updated with header

---
+++ src/app/core/header/header.component.ts
@@ -0,0 +1,30 @@
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-header',
+  templateUrl: './header.component.html',
+  styleUrls: ['./header.component.scss']
+})
+export class HeaderComponent implements OnInit {
+
+  navs = [
+    {
+      label: 'Home',
+      value: '/'
+    },
+    {
+      label: 'UI Playground',
+      value: '/ui-playground'
+    }
+  ];
+
+  constructor(private readonly router: Router) { }
+
+  ngOnInit() {
+  }
+
+  onNavClick(value) {
+    this.router.navigate([value]);
+  }
+}
