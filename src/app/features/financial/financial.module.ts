519711127399c0d2f11db82aaca545d03fd54808
Justin Kennedy <justinjaykennedy@gmail.com>
2019-07-12 12:00:12 -0700

    slowly understanding angular material

--- src/app/features/financial/financial.module.ts
+++ src/app/features/financial/financial.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
+import { MatButtonModule } from '@angular/material/button';
+import { MatDatepickerModule } from '@angular/material/datepicker';
 import { MatInputModule } from '@angular/material/input';
+import { MatNativeDateModule } from '@angular/material/core';
+import { MatRadioModule } from '@angular/material/radio';
+import { MatSelectModule } from '@angular/material/select';
 
 import { FinancialRoutingModule } from './financial-routing.module';
 import { ReceiptComponent } from './receipt/receipt.component';
@@ -11,7 +16,12 @@ import { ReceiptComponent } from './receipt/receipt.component';
   imports: [
     CommonModule,
     FinancialRoutingModule,
-    MatInputModule
+    MatButtonModule,
+    MatDatepickerModule,
+    MatInputModule,
+    MatNativeDateModule,
+    MatRadioModule,
+    MatSelectModule
   ]
 })
 export class FinancialModule { }
